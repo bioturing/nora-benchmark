02b65fcd80f41a3996272706f21c97880182b6a6
vuonghy2442 <vuonghy2442@hotmail.com.vn>
2018-06-22 16:50:58 +0700

    Update runPairedEnd.sh

--- runPairedEnd.sh
+++ runPairedEnd.sh
@@ -3,10 +3,10 @@ THREAD=32
 mkdir $3
 
 mkdir $3/star_rsem
-\time -v -o $3/rsem/time.txt rsem-calculate-expression --no-bam-output --star --star-gzipped-read-file --paired-end -p $THREAD $1 $2 ./index/star_rsem/grch38 $3/star_rsem/result 2> $3/star_rsem/screen.log 
+\time -v -o $3/star_rsem/time.txt rsem-calculate-expression --no-bam-output --star --paired-end -p $THREAD $1 $2 ./index/star_rsem/grch38 $3/star_rsem/result 2> $3/star_rsem/screen.log 
 
 mkdir $3/bowtie2_rsem
-\time -v -o $3/rsem/time.txt rsem-calculate-expression --no-bam-output  --bowtie2 --paired-end -p $THREAD $1 $2 ./index/bowtie2_rsem/grch38 $3/bowtie2_rsem/result 2> $3/bowtie2_rsem/screen.log 
+\time -v -o $3/bowtie2_rsem/time.txt rsem-calculate-expression --no-bam-output  --bowtie2 --paired-end -p $THREAD $1 $2 ./index/bowtie2_rsem/grch38 $3/bowtie2_rsem/result 2> $3/bowtie2_rsem/screen.log 
  
 mkdir $3/kallisto
 \time -v -o  $3/kallisto/time.txt kallisto quant -i ./index/kallisto -o $3/kallisto -t $THREAD $1 $2 2> $3/kallisto/screen.log
